f86a18abc835e7a404dd53215c7dfcf7484375e0
vi.vu001 <viet.vu001@stud.fh-dortmund.de>
2020-03-07 12:55:54 +0100

    stretched buttons to fit the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
 
   // add buttons with color
-  List<Widget> _addButtons(Color _makeColor()) {
+  List<Widget> _addButtons(Color makeColor()) {
     final buttons = <Widget>[];
 
     for (var i = 1; i <= 7; i++) {
-      buttons.add(FlatButton(
-        onPressed: () {
-          final player = AudioCache(); // local variable
-          player.play('note$i.wav');
-        },
-        color: _makeColor(),
+      buttons.add(Expanded(
+        child: FlatButton(
+          onPressed: () {
+            final player = AudioCache(); // local variable
+            player.play('note$i.wav');
+          },
+          color: makeColor()
+        ),
       ));
     }
 
@@ -41,13 +42,12 @@ class XylophoneApp extends StatelessWidget {
     // generate random color
     Color _makeColor() {
       int random = new Random().nextInt(colors.length);
-      print(random);
       return colors.removeAt(random);
     }
 
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: _addButtons(_makeColor),
